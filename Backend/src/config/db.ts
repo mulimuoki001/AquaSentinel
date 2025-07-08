e8811bcb6b0feeb83eeb88c0c52f499e0520a475
mulimuoki001 <b.muoki@alustudent.com>
2025-07-08 03:33:31 +0200

    Railway DB url

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -1,31 +1,3 @@
-// import dotenv from "dotenv";
-// import { Pool } from "pg";
-
-// dotenv.config();
-
-// export const createDbPool = async (database: string) => {
-//   const defaultPool = new Pool({
-//     host: process.env.DB_HOST,
-//     port: parseInt(process.env.DB_PORT || "5432"),
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME || "aquasentineldb",
-//   });
-
-
-
-//   const pool = new Pool({
-//     host: process.env.DB_HOST,
-//     port: parseInt(process.env.DB_PORT || "5432"),
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database,
-//   });
-
-//   return pool;
-// };
-
-// export const db = createDbPool(process.env.DB_NAME!);
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
@@ -33,23 +5,16 @@ dotenv.config();
 
 let db: Pool;
 
-if (process.env.NODE_ENV === 'production') {
-  // Running on Railway (cloud)
-  db = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  // Running locally
-  db = new Pool({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432"),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || "aquasentineldb"
-  });
-}
+
+// Running on Railway (cloud)
+db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+
 
 export { db };
